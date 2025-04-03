860cd0e6e02b87f897b5c86eb07d237551aa0fa7
gurpreetSingh4 <gurpreetyarasingh@gmail.com>
2025-04-04 02:01:11 +0530

    modify oauth scope

--- src/controllers/oAuth-controller.js
+++ src/controllers/oAuth-controller.js
@@ -60,7 +60,6 @@ export function getGoogleOAuthUrl(){
         scope: [
             'https://www.googleapis.com/auth/userinfo.profile',
             'https://www.googleapis.com/auth/userinfo.email',
-            'https://www.googleapis.com/auth/documents.readonly',
         ].join(' '),
         access_type: 'offline',  // Required to get refresh token
         prompt: 'consent'
